5262efce6cf50887891ec0f72c80e6ef93b8b6f3
EC2 Default User <ec2-user@ip-172-31-88-238.ec2.internal>
2022-06-12 15:53:26 +0000

    220613_bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  
+  resources :postimages, only: [:new, :index, :show]
+  
   devise_for :users
   root to: 'homes#top'
-  
-  resources :post_images, only: [:new, :index, :show]  
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_05_083307) do
+ActiveRecord::Schema.define(version: 2022_06_11_010005) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_images", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
