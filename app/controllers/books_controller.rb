9de6366969b79259ca73a43b1e8c86d22402cc48
EC2 Default User <ec2-user@ip-172-31-88-238.ec2.internal>
2022-06-26 11:36:34 +0000

    220626_bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,32 @@
 class BooksController < ApplicationController
+
+  def index
+    @book = Book.new
+    
+    @books = Book.all  
+    
+    @user = current_user
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+  end
+
+
+  
+  private 
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+      
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "/homes/about" => "homes#about", as: "about"
-  get "/books/top" => "books#top", as: "books"
   get "/users/index" => "users#index", as: "users"
+  
 
   resources :postimages, only: [:new, :index, :show]
+  resources :books
   
 end
